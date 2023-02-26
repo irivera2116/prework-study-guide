9c293736b57b3bad3595eaa774eb867c4abe0efa
irivera2116 <irivera2116@GMAIL.COM>
2023-02-26 14:06:04 -0500

    <called a function using javascript to navigate the topics>

--- assets/script.js
+++ assets/script.js
@@ -20,5 +20,7 @@ function selectTopic() {
    console.log('Please try again!');
  }
 }
-listTopics()
-selectTopic()
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
